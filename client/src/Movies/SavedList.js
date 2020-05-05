b6fa1576c36231a5e9a5deb878b9e289a5556cac
HoolioCoolio <hoolio.osuna@gmail.com>
2020-05-04 22:20:02 -0700

    completed mvp time to stretch

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+const SavedList = props => {
+  let history = useHistory();
+  
+    function handleClick() {
+      history.push("/");
+    }
+
+
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">{movie.title}</span>
+      ))}
+      <div onClick={handleClick} className="home-button">
+        {/* link and push could also work here*/}
+        Home
+      </div>
+    </div>
+  );
+};
 
 export default SavedList;
